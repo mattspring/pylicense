82746dcaece51a3a4e6e007bc16b0f09a6ff395f
Matt Smith <matt@springlabs.com>
2019-02-06 18:36:01 -0800

    added dependencies to install_requires

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 
 setup(
-  name='pylicense',
-  license='MIT',
-  version='0.1',
-  py_modules=['pylicense'],
+    name='pylicense',
+    license='MIT',
+    version='0.1',
+    py_modules=['pylicense'],
+    install_requires=[
+        'bs4',
+        'lxml'
+    ],
 )
